6cfc30332c00787ac62e4d922bd331b0b4a3d772
Kyle Linhardt <kylelinhardt@Kyles-iMac.local>
2017-10-24 21:59:59 -0400

    added user repository

--- src/main/java/com/dayshare/user/UserRepository.java
+++ src/main/java/com/dayshare/user/UserRepository.java
@@ -1,4 +1,14 @@
 package com.dayshare.user;
 
-public interface UserRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    @RestResource(rel = "username-contains", path = "containsUsername")
+    Page<User> findByUsernameContaining(@Param("username") String username, Pageable page);
+
 }
